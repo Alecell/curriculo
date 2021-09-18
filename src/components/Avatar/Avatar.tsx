56e99409f27b88b816aadb1b00af69f5f5ea6c0a
Alecell <alecell.dev@gmail.com>
2021-09-18 02:27:04 -0300

    change external image request to locally loaded

--- src/components/Avatar/Avatar.tsx
+++ src/components/Avatar/Avatar.tsx
@@ -1,10 +1,12 @@
 import scss from './Avatar.module.scss';
 
+import photo from './images/alexandre-gomes-pequeno.png';
+
 function Avatar() {
   return (
     <section className={scss.container}>
       <figure className={scss.imageContainer}>
-        <img className={scss.image} src="https://alecell.github.io/images/alexandre-gomes-pequeno.png" alt="" />
+        <img className={scss.image} src={photo} alt="" />
       </figure>
       <h1 className={scss.title}>
         ALEXANDRE GOMES
